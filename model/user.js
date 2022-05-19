75d24ee3d9aec0531d1201023cdd3a5b2f3e3935
Hamza <boujahamza@gmail.com>
2022-05-19 11:42:04 +0100

    added user schema and DB connection

--- model/user.js
+++ model/user.js
@@ -1,1 +1,9 @@
 const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, default: "normal" }, // normal role by default, admin for higher privileges
+    token: { type: String }
+})
