2e2142c13149c9961f27ef25e963431955896005
Hamza <boujahamza@gmail.com>
2022-06-04 16:34:13 +0100

    added article and event count proxy

--- routes/articlesRouter.js
+++ routes/articlesRouter.js
@@ -8,6 +8,7 @@ const EventRouter = express.Router();
 const httpProxy = require('express-http-proxy');
 
 const auth = require('../middleware/auth');
+const validateRole = require('../middleware/roleValidation');
 
 const Proxy = httpProxy('http://localhost:3000', {
     proxyReqOptDecorator: function (proxyReqOpts, srcReq) {
@@ -18,22 +19,24 @@ const Proxy = httpProxy('http://localhost:3000', {
     }
 })
 ArticleRouter.get("/article", (req, res, next) => { Proxy(req, res, next); })
+    .get("/article/count/", auth, validateRole, (req, res, next) => { Proxy(req, res, next); })
     .get("/article/:article_id", (req, res, next) => { Proxy(req, res, next); })
     .get("/article/:article_id/comment", (req, res, next) => { Proxy(req, res, next); })
-    .get("/article/:user_id/article",  (req, res, next) => { Proxy(req, res, next); })
-    .post("/article", auth,(req, res, next) => {Proxy(req, res, next) })
-    .post("/article/:article_id/comment",auth, (req, res, next) => { //Need to add middleware to validate role
+    .get("/article/:user_id/article", (req, res, next) => { Proxy(req, res, next); })
+    .post("/article", auth, (req, res, next) => { Proxy(req, res, next) })
+    .post("/article/:article_id/comment", auth, (req, res, next) => { //Need to add middleware to validate role
         Proxy(req, res, next);
     })
 
 ImageRouter
     .get("/images", (req, res, next) => { Proxy(req, res, next); })
-    .post("/images", (req, res, next) => {Proxy(req, res, next); })
+    .post("/images", (req, res, next) => { Proxy(req, res, next); })
 
 EventRouter
- .get('/events/:user_id',(req, res, next) => { Proxy(req, res, next); })
- .post("/events", (req, res, next) => {Proxy(req, res, next); })
- .post("/events/:event_id", (req, res, next) => {Proxy(req, res, next); })
+    .get("/events/count/", auth, validateRole, (req, res, next) => { Proxy(req, res, next); })
+    .get('/events/:user_id', (req, res, next) => { Proxy(req, res, next); })
+    .post("/events", (req, res, next) => { Proxy(req, res, next); })
+    .post("/events/:event_id", (req, res, next) => { Proxy(req, res, next); })
 
 
 module.exports = {
