3c31f215c2045924478de66f5cc01912533867c5
Hamza <boujahamza@gmail.com>
2022-06-06 18:50:25 +0100

    added feedRouter

--- routes/gameReviewsRouter.js
+++ routes/gameReviewsRouter.js
@@ -24,6 +24,10 @@ router.get("/count/reviews", auth, validateRole, (req,res,next) => {
     gameReviewsProxy(req,res,next);
 })
 
+router.get("/user/:id/reviews", auth, validateRole, (req,res,next) => {
+    gameReviewsProxy(req,res,next);
+})
+
 router.get("/", (req,res,next)=>{
     gameReviewsProxy(req,res,next);
 });
